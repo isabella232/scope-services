ec863b17ffdc5db65f38f70b398d6352c03c82be
Jan Borsodi <jborsodi@opera.com>
2009-01-05 14:07:46 +0100

    Fixed names of Ecmascript Logger messages to better reflect their content (ie. not commands).

--- ecmascript_logger.py
+++ ecmascript_logger.py
@@ -3,18 +3,18 @@ from opprotoc.proto import Proto, Quantifier, Field, Message, Request, Event, Se
 
 # Service: EcmascriptLogger
 # Is this service even used, and does it even work?
-esl_new_script = Message("NewScript",
-                         fields=[Field(Proto.String, "context",    1)
-                                ,Field(Proto.Uint32, "url",        2)
-                                ,Field(Proto.String, "source",     3)
+esl_new_script = Message("ScriptInfo",
+                         fields=[Field(Proto.String, "context", 1)
+                                ,Field(Proto.String, "url",     2)
+                                ,Field(Proto.String, "source",  3)
                                 ])
 
-esl_configure = Message("Configure",
+esl_configure = Message("Config",
                         fields=[Field(Proto.Bool, "reformat", 1, q=Quantifier.Optional)
                                ])
 
 ecmascript_logger = Service("EcmascriptLogger", version="2.0", coreRelease="2.4",
-                            commands=[Request(1,  "Configure", esl_configure, False)
-                                     ,Event(0,  "NewScript", esl_new_script)
+                            commands=[Request(1, "Configure", esl_configure, False)
+                                     ,Event(0,   "OnNewScript", esl_new_script)
                                      ],
                             cpp_class="OpScopeEcmascriptLogger", cpp_hfile="modules/scope/src/scope_ecmascript_logger.h")
