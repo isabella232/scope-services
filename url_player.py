3b9b6fcfbed93148da6719eb3d86fac688cb6d8e
Jan Borsodi <jborsodi@opera.com>
2009-01-06 14:08:08 +0100

    Cleaned up Url Player service to make it better reflect the current code and not the spec.

--- url_player.py
+++ url_player.py
@@ -4,21 +4,23 @@ from opprotoc.proto import Proto, Quantifier, Field, Message, Request, Event, Se
 # Service: UrlPlayer
 # TODO: This service should probable be replaced with the Exec service.
 
-urlplayer_window_data = Message("WindowData",
-                                fields=[Field(Proto.Uint32, "windowCount", 1)
-                                       ])
+window_count = Message("WindowCount",
+                       fields=[Field(Proto.Uint32, "windowCount", 1)
+                              ])
 
-urlplayer_window_result = Message("WindowResult",
-                                 fields=[Field(Proto.Uint32, "windowCount", 1)
-                                       ])
-
-urlplayer_play = Message("UrlSelection",
+request = Message("Request",
                          fields=[Field(Proto.Uint32, "windowNumber", 1)
                                 ,Field(Proto.String, "url",          2)
                                 ])
 
+window = Message("Window",
+                 fields=[Field(Proto.Uint32, "windowID", 1)
+                        ])
+
 url_player = Service("UrlPlayer", version="2.0", coreRelease="2.4",
-                     commands=[Request(1,  "CreateWindows", urlplayer_window_data, urlplayer_window_result)
-                              ,Request(3,  "LoadUrl",       urlplayer_play,        False)
+                     commands=[Request(1, "CreateWindows",      window_count, window_count)
+                              ,Request(2, "LoadUrl",            request,      window)
+                              ,Event(3,   "OnUrlLoaded",        window)
+                              ,Event(4,   "OnConnectionFailed", window)
                               ],
                      cpp_class="OpScopeUrlPlayer", cpp_hfile="modules/scope/src/urlplayer_command.h")
