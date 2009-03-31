aea5ba1dc2b846edc2ef62d83fc56fa9c2e175eb
Jan Borsodi <jborsodi@opera.com>
2009-03-31 16:08:07 +0200

    Added a description of the "scope" service which is now done entirely using UMS.

--- scope.py
+++ scope.py
@@ -1,24 +1,68 @@
 # OpProtoc
 from opprotoc.proto import Proto, Quantifier, Field, Message, Request, Event, Service
 
-scope_message = Message("ScopeMessage",
-                          fields=[Field(Proto.String, "message",    1)])
+client_info = Message("ClientInfo",
+                      fields=[Field(Proto.String, "format", 1)
+                             ])
 
-scope = Service(
-    "Scope", 
-    version="1.0", 
-    coreRelease="2.4",
-    commands=[
-        Event(0,  "OnServices",  scope_message),
-        Event(1,  "OnConnect",  scope_message),
-        Event(2,  "OnQuit",  scope_message),
-        Event(3,  "OnConnectionLost",  scope_message),
-        Request(4,  "Enable",       scope_message,      scope_message),
-        Request(5,  "Disable",       scope_message,      scope_message),
-        Request(6,  "Configure",       scope_message,      scope_message),
-        Request(7,  "Info",       scope_message,      scope_message),
-        Request(8,  "Quit",       scope_message,      scope_message),                       
-        ],
-    cpp_class="", 
-    cpp_hfile=""
-    )
+service = Message("Service",
+                  fields=[Field(Proto.String, "name",          1)
+                         ,Field(Proto.String, "version",       2)
+                         ,Field(Proto.Uint32, "activeClients", 3)
+                         ,Field(Proto.Uint32, "maxClients",    4)
+                         ])
+
+command = Message("CommandInfo",
+                  fields=[Field(Proto.String, "name",   1)
+                         ,Field(Proto.Uint32, "number", 2)
+                         ])
+
+event = Message("EventInfo",
+                fields=[Field(Proto.String, "name",   1)
+                       ,Field(Proto.Uint32, "number", 2)
+                       ])
+
+service_info = Message("ServiceInfo",
+                       fields=[Field(Proto.Message, "commands", 1, message=command, q=Quantifier.Repeated)
+                              ,Field(Proto.Message, "events",   2, message=event, q=Quantifier.Repeated)
+                              ])
+
+service_selection = Message("ServiceSelection",
+                            fields=[Field(Proto.String, "name", 1)
+                                   ])
+
+host_info = Message("HostInfo",
+                      fields=[Field(Proto.Uint32,  "clientID",        1)
+                             ,Field(Proto.Uint32,  "stpVersion",      2)
+                             ,Field(Proto.String,  "coreVersion",     3)
+                             ,Field(Proto.String,  "platform",        4)
+                             ,Field(Proto.String,  "operatingSystem", 5)
+                             ,Field(Proto.String,  "userAgent",       6)
+                             ,Field(Proto.Message, "services",        7, message=service, q=Quantifier.Repeated)
+                             ])
+
+client_id = Message("ClientID",
+                    fields=[Field(Proto.String, "uuid", 1)
+                           ])
+
+service_list = Message("ServiceList",
+                       fields=[Field(Proto.String, "service",    1, q=Quantifier.Repeated)
+                              ])
+
+error_info = Message("ErrorInfo",
+                     fields=[Field(Proto.String, "description", 1)
+                            ])
+
+window_manager = Service("Scope", version="1.0", coreRelease="2.4",
+                         commands=[Request(3, "Connect",    client_info,       host_info, cpp_send_message=True)
+                                  ,Request(4, "Disconnect", client_id,         False)
+                                  ,Request(5, "Enable",     service_selection, False)
+                                  ,Request(6, "Disable",    service_selection, False)
+                                  ,Request(7, "Info",       service_selection, service_info)
+                                  ,Request(8, "Quit",       False,             False)
+                                  ,Event(0, "OnServices",       service_list)
+                                  ,Event(1, "OnQuit",           False)
+                                  ,Event(2, "OnConnectionLost", False)
+                                  ,Event(9, "OnError",          error_info)
+                                  ],
+                         cpp_class="OpScopeProtocolService", cpp_hfile="modules/scope/src/scope_protocol_service.h")
