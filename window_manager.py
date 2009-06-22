9bbf1e683a541d9e7abe21c29f19ef010d1c9b5b
Jan Borsodi <jborsodi@opera.com>
2009-06-22 16:23:13 +0200

    Removing some old XML examples from WindowManager file.

--- window_manager.py
+++ window_manager.py
@@ -24,48 +24,6 @@ window_filter = Message("WindowFilter",
                                ,Field(Proto.String, "excludePatternList", 5, q=Quantifier.Repeated)
                                ])
 
-# Core 2.2 examples
-window_list_c22 = """<list-windows-reply>
-  <window-info>
-    <window-id>5</window-id>
-    <title>node</title>
-    <window-type>body</window-type>
-    <opener-id>7</opener-id>
-  </window-info>
-</list-windows-reply>
-"""
-
-filter_c22 = """<filter>
-  <clear>1</clear>
-  <include>
-    <window-id>7</window-id>
-  </include-id>
-  <include>
-    <string>html</string>
-  </include-pattern>
-  <include>
-    <string>dom</string>
-  </include-pattern>
-  <include>
-    <string>node</string>
-  </include-pattern>
-  <exclude>
-    <string>body</string>
-  </exclude-pattern>
-</filter>
-"""
-updated_window_c22="""<updated-window>
-  <window-id>7</window-id>
-  <title>html</title>
-  <window-type>dom</window-type>
-  <opener-id>10</opener-id>
-</updated-window>
-"""
-window_closed_c22="""<window-closed>
-  <window-id>1</window-id>
-</window-closed>
-"""
-
 window_manager = Service("WindowManager", version="2.0", coreRelease="2.4",
                          commands=[Request(1, "GetActiveWindow", False,         window_id)
                                   ,Request(3, "ListWindows",     False,         window_list) # core_2_2=window_list_c22
