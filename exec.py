95bb1a6a5c46eab70c9b5a2511416ba9e5e89e81
Jan Borsodi <jborsodi@opera.com>
2009-07-24 14:56:29 +0200

    Updated exec service with new features. Fixes task CORE-22546.

--- exec.py
+++ exec.py
@@ -47,22 +47,80 @@ area = Message("Area",
                        Field(Proto.Int32, "w", 3),
                        Field(Proto.Int32, "h", 4)])
 
+color_spec_doc="""Specifies a new color specification.
+The `id` is used when reporting back the results.
+You can have overlapping color specifications.
+Note: There can be a maximum of 16 color specifications!
+
+Color values ranges from 0 (no color) to 255 (maximal saturation), other values are not allowed.
+Default (meaning field missing) is 0 for *Low elements and 255 for *High fields.
+"""
+
+color_spec = Message("ColorSpec", doc=color_spec_doc, is_global=False,
+                     fields=[Field(Proto.Uint32, "id", 1),
+                             Field(Proto.Uint32, "redLow",    2, q=Quantifier.Optional),
+                             Field(Proto.Uint32, "redHigh",   3, q=Quantifier.Optional),
+                             Field(Proto.Uint32, "greenLow",  4, q=Quantifier.Optional),
+                             Field(Proto.Uint32, "greenHigh", 5, q=Quantifier.Optional),
+                             Field(Proto.Uint32, "blueLow",   6, q=Quantifier.Optional),
+                             Field(Proto.Uint32, "blueHigh",  7, q=Quantifier.Optional),
+                             ])
+
 screenwatcher_data = Message("ScreenWatcher",
-                             fields=[Field(Proto.Uint32, "timeOut",  1),
-                                     Field(Proto.Message, "area",    2, message = area),
-                                     Field(Proto.String, "md5List",  3, q=Quantifier.Repeated),
-                                     Field(Proto.Uint32, "windowID", 4, q=Quantifier.Optional)])
+                             fields=[Field(Proto.Uint32,  "timeOut",       1, doc="Number of milliseconds to wait before capturing the screen area."),
+                                     Field(Proto.Message, "area",          2, message = area),
+                                     Field(Proto.String,  "md5List",       3, q=Quantifier.Repeated),
+                                     Field(Proto.Uint32,  "windowID",      4, q=Quantifier.Optional, doc="The ID of the window to watch, the default (or 0) is to watch the current window"),
+                                     Field(Proto.Message, "colorSpecList", 5, q=Quantifier.Repeated, message=color_spec),
+                                     ])
+
+color_match = Message("ColorMatch", is_global=False,
+                      fields=[Field(Proto.Uint32, "id",    1, doc="The `ColorSpec.id` which matched a color"),
+                              Field(Proto.Uint32, "count", 2),
+                              ])
 
 screenwatcher_event = Message("ScreenWatcherEvent",
-                              fields=[Field(Proto.Uint32, "windowID", 1),
-                                      Field(Proto.String, "md5", 2, q=Quantifier.Optional),
-                                      Field(Proto.Bytes, "png", 3, q=Quantifier.Optional)])
+                              fields=[Field(Proto.Uint32,  "windowID",       1, doc="The ID of the window that was triggered by a screen watch, or 0 if the screen watch failed or was cancelled"),
+                                      Field(Proto.String,  "md5",            2),
+                                      Field(Proto.Bytes,   "png",            3, q=Quantifier.Optional),
+                                      Field(Proto.Message, "colorMatchList", 4, q=Quantifier.Repeated, message=color_match),
+                                      ])
+
+mouse_action = Message("MouseAction",
+                       fields=[Field(Proto.Uint32, "windowID", 1),
+                               Field(Proto.Int32,  "x", 2),
+                               Field(Proto.Int32,  "y", 3),
+                               Field(Proto.Uint32, "buttonAction", 4,
+                                     doc="""`buttonAction` specifies the buttons to press or release
+It is specifies as the sum of button actions:
+      1 - Button 1 down
+      2 - Button 1 up
+
+      4 - Button 2 down
+      8 - Button 2 up
+
+     16 - Button 3 down
+     32 - Button 3 up
+
+For example, to press button 1 and release button 2, the value is 9 (1+8)
+
+Buttons are clicked in the sequence listed above. Note that down actions are
+listed before up actions, thus allowing single-clicking with one command
+(e.g. using value 3)"""),
+                               ])
+
+mouse_doc="""Move mouse to the given position on the screen/window.
+Note that the mouse cursor is not moved visibly.
+
+The coordinates are relative to the upper left corner of the tab
+(not including chrome)."""
 
 exec_service = Service("Exec", version="2.0", coreRelease="2.4",
                        doc="""The Opera Exec protocol can be used to control an Opera instance from\nthe outside, and various operations can be initiated. This\nfunctionality is mainly useful for QA testing.""",
                        commands=[Request(1, "Exec", exec_data, False),
                                  Request(2, "GetActionInfoList", False, get_action_list),
                                  Request(3, "SetupScreenWatcher", screenwatcher_data, False),
-                                 Event(4, "OnScreenWatcherEvent", screenwatcher_event)
+                                 Event(4,   "OnScreenWatcherEvent", screenwatcher_event),
+                                 Request(5, "SendMouseAction", mouse_action, False, doc=mouse_doc),
                                 ],
                        cpp_class="OpScopeExec", cpp_hfile="modules/scope/src/scope_exec.h")
